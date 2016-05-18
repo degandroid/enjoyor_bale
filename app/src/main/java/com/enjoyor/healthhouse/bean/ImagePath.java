79505b5394c8dcd73f22a487398ba4b75c1ec2bf
degandroid <degandroid@163.com>
2016-05-18 11:04:46 +0800

    no message

---
+++ app/src/main/java/com/enjoyor/healthhouse/bean/ImagePath.java
@@ -0,0 +1,20 @@
+package com.enjoyor.healthhouse.bean;
+
+/**
+ * Created by Administrator on 2016/5/18.
+ */
+public class ImagePath {
+//    "article": null,
+//            "createTime": null,
+//            "id": null,
+//            "path": "files/res/app/start/01d02750-be7f-4a04-ab6a-221c2cfff215.JPG"
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
