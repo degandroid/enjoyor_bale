36eeb0f0ff30bb5d8cddbb0cb90eb3bc8b9d212b
perfect <18919028649@163.com>
2016-06-27 15:49:32 +0800

    no message

---
+++ app/src/main/java/com/enjoyor/healthhouse/bean/VersionInfo.java
@@ -0,0 +1,31 @@
+package com.enjoyor.healthhouse.bean;
+
+/**
+ * Created by YuanYuan on 2016/6/27.
+ */
+public class VersionInfo {
+
+    /**
+     * url : http://www.cnbale.com/KJkj23hHg
+     * version : 1.0.0
+     */
+
+    private String url;//下载地址
+    private String version;//版本号
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
